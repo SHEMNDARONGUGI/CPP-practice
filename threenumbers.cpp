76bae248f8a1440dbac09e11698b0fb2f37b52b9
SHEMNDARONGUGI <ndaroshem@gmail.com>
2024-11-07 12:03:07 +0300

    Initial commit

---
+++ threenumbers.cpp
@@ -0,0 +1,28 @@
+//a programme that takes in two numbers and displays the greatest
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cout<<"INPUT FIRST INTEGER"<<endl;
+    cin>>a;
+    cout<<"INPUT SECOND INTEGER"<<endl;
+    cin>>b;
+    cout<<"INPUT THIRD NUMBER"<<endl;
+    cin>>c;
+    if(a > c && a>b){
+        cout<<a<<" is bigger in the numbers given"<<endl;
+    }
+    else if(c>a and c>b){
+        cout<<c<<" is bigger in the the numbers given"<<endl;
+    }
+    else{
+        cout<<b<<" is bigger in the numbers give"<<endl;
+
+    }
+    
+    
+}
