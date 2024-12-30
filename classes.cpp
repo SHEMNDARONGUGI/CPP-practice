75b7345b86f6eb5aa1d71d067b030aefef13d2f9
SHEMNDARONGUGI <ndaroshem@gmail.com>
2024-12-30 20:41:41 +0300

    Initial commit

--- classes.cpp
+++ classes.cpp
@@ -18,14 +18,14 @@ public:
 int main()
 {
     Employee employee1;
-    // employee1.name = "Shem";
-    // employee1.EmployeeNo = "ICT-1201";
-    // employee1.Age = 23;
+    employee1.name = "Shem";
+    employee1.EmployeeNo = "ICT-1201";
+    employee1.Age = 23;
     employee1.IntroduceYourself();
 
     Employee employee2;
-    // employee2.name = "Steve";
-    // employee2.EmployeeNo = "HR-2341";
-    // employee2.Age = 35;
+    employee2.name = "Steve";
+    employee2.EmployeeNo = "HR-2341";
+    employee2.Age = 35;
     employee2.IntroduceYourself();
 }
