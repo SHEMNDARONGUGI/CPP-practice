47bff40e446086f713a1934435269bd532d43cc5
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-01-11 14:35:04 +0300

    Initial commit

---
+++ practice1.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    cout<< "Hello world"<<endl;
+    cout<<"Name"<<setw(15)<<"Age"<<setw(15)<<"Phone"<<endl;
+    return 0;
+    
+}
